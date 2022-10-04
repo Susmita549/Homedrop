45c71cfcaa2d70a5d8dc21a1a8e81f346b243b9b
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-10-04 16:28:49 +0530

    sidebar

--- homedrop/src/components/Sidebar.jsx
+++ homedrop/src/components/Sidebar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import styles from "./sidebar.module.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className={styles.sidebar}>
+        <div className={styles.mainDiv}>
+          <div className={styles.icon}>{<AiOutlineHome />}</div>
+          <div className={styles.name}>Overview</div>
+        </div>
+        <div className={styles.mainDiv}>
+          <div className={styles.icon}>{<FiShoppingCart />}</div>
+          <div className={styles.name}>Sales</div>
+        </div>
+        <div className={styles.mainDiv}>
+          <div className={styles.icon}>{<AiOutlineHome />}</div>
+          <div className={styles.name}>Purchases</div>
+        </div>
+        <div className={styles.mainDiv}>
+          <div className={styles.icon}>{<AiOutlineHome />}</div>
+          <div className={styles.name}>Inventory</div>
+        </div>
+        <div className={styles.mainDiv}>
+          <div className={styles.icon}>{<AiOutlineHome />}</div>
+          <div className={styles.name}>Marketing</div>
+        </div>
+        <div className={styles.mainDiv}>
+          <div className={styles.icon}>{<AiOutlineHome />}</div>
+          <div className={styles.name}>Analytics</div>
+        </div>
+        <div className={styles.mainDiv}>
+          <div className={styles.icon}>{<AiOutlineHome />}</div>
+          <div className={styles.name}>Staff</div>
+        </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
